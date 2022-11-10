ff7253356406e0d2b60baad043a6c54d834d55bb
waflol <clark.le.goldenowl@gmail.com>
2022-11-10 23:49:38 +0700

    add assets image and api user

---
+++ api/user/tests/test_user_api.py
@@ -0,0 +1,6 @@
+from django.test import TestCase  # noqa
+from django.contrib.auth import get_user_model  # noqa
+from django.urls import reverse # noqa
+
+from rest_framework.test import APIClient   # noqa
+from rest_framework import status   # noqa
--- api/user/views.py
+++ api/user/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render  # noqa
 from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
+from rest_framework import generics
+from rest_framework.parsers import MultiPartParser
 
 
 # Create your views here.
-class UserApiViewSet(viewsets.ModelViewSet):
+# class UserApiViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.filter(is_active=True)
+#     serializer_class = UserSerializer
+
+class UserApiViewSet(viewsets.ViewSet,
+                     generics.ListAPIView,
+                     generics.CreateAPIView,
+                     generics.RetrieveAPIView):
+    """
+    API:
+        * đăng ký user - generics.CreateAPIView
+        * lấy thông tin user - generics.RetrieveAPIView
+        * lấy danh sách user - generics.ListAPIView
+    """
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
+    parser_classes = [MultiPartParser, ]
