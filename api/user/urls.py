d7898c0f6ae3617f1f7481bcbff0b0758bede31d
ClarkLe01 <clark.le.goldenowl@gmail.com>
2022-11-16 13:20:32 +0700

    urls update

--- api/user/urls.py
+++ api/user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include  # noqa
 from . import views
 from rest_framework.routers import DefaultRouter
 app_name = 'user'
-router = DefaultRouter(app_name="test")
+router = DefaultRouter()
 router.register(r'users', views.UserListApiViewSet, basename='users')
 router.register(r'profile', views.ProfileUserApiViewSet, basename='profile')
 
